22dfd855e1419b0be047c47da1849af62b99a307
Jazzy-girl <rsw5713@g.rit.edu>
2026-03-30 21:09:23 -0400

    made test file

--- I2C/I2C.ino
+++ I2C/I2C.ino
@@ -2,18 +2,22 @@
 
 #include <Wire.h>
 
-byte tXByte = 0xAA;
+byte tXByte = 0x0123456789ABCDEF01234567;
+#define address (0x52)
 
-void I2C_TxHandler(void){
+void I2C_TxHandler(void)
+{
   Wire.write(TxByte);
 }
 
-void setup() {
-  Wire.begin(0x52); // Init I2C (Slave Mode: addr = 0x52)
+void setup()
+{
+  Wire.begin(address); // Init I2C (Slave Mode: addr = 0x52)
   Wire.onRequest(I2C_TxHandler);
 }
 // New!
-void loop() {
+void loop()
+{
   // Nothing to be done here
   // Test: Generate multiple CAN messages
 }
